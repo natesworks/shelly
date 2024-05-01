e7f921e193d24cff3403b0be311e0f79b679d372
Natesworks <natesworks@proton.me>
2024-05-01 16:39:33 +0200

    Add set prompt command

--- shelly.cpp
+++ shelly.cpp
@@ -3,11 +3,9 @@
 #include <cstring>
 #include <sys/wait.h>
 #include <unistd.h>
-#include <vector>
 #include <sstream>
-#ifdef _WIN32
-#include <windows.h>
-#endif
+#include <vector>
+#include <sys/stat.h>
 
 using namespace std;
 
@@ -16,7 +14,15 @@ string prompt;
 string input;
 
 int writeConfig(const string& prompt) {
-    configFile.open("config", ios::out);
+    string directoryPath = "/etc/shelly";
+
+    if (access(directoryPath.c_str(), F_OK) != 0) {
+        if (mkdir(directoryPath.c_str(), 0755) != 0) {
+            return 0;
+        }
+    }
+
+    configFile.open("/etc/shelly/config", ios::out);
     if (configFile.is_open()) {
         configFile << prompt;
         configFile.close();
@@ -26,7 +32,7 @@ int writeConfig(const string& prompt) {
 }
 
 int readConfig() {
-    configFile.open("config", ios::in);
+    configFile.open("/etc/shelly/config", ios::in);
     if (configFile.is_open()) {
         getline(configFile, prompt);
         configFile.close();
@@ -36,49 +42,44 @@ int readConfig() {
 }
 
 int main(int argc, char* argv[]) {
-    if (argc >= 4 && strcmp(argv[1], "set") == 0 && strcmp(argv[2], "prompt") == 0) {
-        return writeConfig(argv[3]);
-    }
-
     if (!readConfig()) {
+        // TO:DO make a setup wizard so lazy people dont have to go through the pain of running a command
         cout << "Config file not found or empty. Defaulting to \"$ \"." << endl;
+        cout << "Run \"set prompt\" to set a prompt." << endl;
         prompt = "$ ";
-        writeConfig(prompt);
     }
 
     while(true) {
         cout << prompt;
         getline(cin, input);
-
-        #ifdef _WIN32
-            system(input.c_str());
-        #else
-            if (!input.empty()) {
-                // i have no idea what the fuck im doing
-                istringstream iss(input);
-                vector<char*> args;
-                string arg;
-                while (iss >> arg) {
-                    args.push_back(strdup(arg.c_str()));
-                }
-                args.push_back(nullptr); 
-
-                // had to make it more complicated cuz the easy way exited the program after running a command even in while true loop
-                pid_t pid = fork();
-                if (pid == 0) {
-                    execvp(args[0], args.data());
-                    exit(1);
-                } else if (pid > 0) {
-                    wait(NULL);
-                } else {
-                    cout << "Failed to fork process." << endl;
-                }
-
-                for (auto& arg : args) {
-                    free(arg);
-                }
+        if (input.find("set prompt ") == 0) {
+            prompt = input.substr(11);
+        }
+        else if (!input.empty()) {
+            // i have no idea what the fuck im doing
+            istringstream iss(input);
+            vector<char*> args;
+            string arg;
+            while (iss >> arg) {
+                args.push_back(strdup(arg.c_str()));
             }
-        #endif
+            args.push_back(nullptr); 
+
+            // had to make it more complicated cuz the easy way exited the program after running a command even in while true loop
+            pid_t pid = fork();
+            if (pid == 0) {
+                execvp(args[0], args.data());
+                exit(1);
+            } else if (pid > 0) {
+                wait(NULL);
+            } else {
+                cout << "Failed to fork process." << endl;
+            }
+
+            for (auto& arg : args) {
+                free(arg);
+            }
+        }
     }
     return 1;
 }
